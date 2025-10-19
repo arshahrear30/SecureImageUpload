2b79f69ba8e7042be3711a0ba99254232e5a823f
A R Shahrear <arshahrear30@gmail.com>
2025-10-19 19:41:14 +0600

    Update 3062 secure.php

--- 3062 secure.php
+++ 3062 secure.php
@@ -1,9 +1,10 @@
 <?php
 $image = $_POST['image'];
 $decodedImage = base64_decode($image);
-// image name & path
-$fileName = time() . '_' . rand(1000, 10000) . '.jpg';
-$imagePath = "images/" . $fileName;
+// image name & path //php tay 2 ta string dot . dia jukto koraty hoy .. 
+//rand =random ..that mean 1-1000 ar modde akta number nibay karon user akoi somoy onek jon astay paray ...
+$fileName = time() . '_' . rand(1, 1000) . '.jpg';//time = mili second convert hoilo .. //under score dici নিজের সুবিদায় জাতে নামের পর number দিলে buja jay
+$imagePath = "images/" . $fileName; //php tay images name a ekta folder create kori...
 if (isset($image)) {
 if (file_put_contents($imagePath, $decodedImage)) {
 echo "image সঠিক ভাবে upload হয়েছে।";
